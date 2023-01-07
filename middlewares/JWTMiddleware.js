b79673bf21c9fbaad7824dc9c0e2b715044fa1e6
BudiGabe <razvan.gabriel.budaca@gmail.com>
2023-01-07 18:45:28 +0200

    Updated base project

--- middlewares/JWTMiddleware.js
+++ middlewares/JWTMiddleware.js
@@ -1,25 +1,23 @@
+const jwt = require('jsonwebtoken');
+const JWT_KEY = '1158659639IFIUHSDIUSDF';
+
 const JWTMiddleware = (request, response, next) => {
-    const authorization = request.headers.authorization;
-    if(!authorization) {
-        response.status(403).send({
-            message: 'Unauthorized',
-        });
-        return;
-    }
+  const authorization = request.headers.authorization;
+  if(!authorization) {
+      next();
+      return;
+  }
 
-    const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');    
 
-    try {
-        const tokenPayload = jwt.verify(token, JWT_KEY);
-        request.tokenPayload = tokenPayload;
-
-        next();
-    } catch (e) {
-        response.status(403).send({
-            message: 'Unauthorized',
-        });
-        return;
-    }
+  try {
+      const tokenPayload = jwt.verify(token, JWT_KEY);
+      request.tokenPayload = tokenPayload;
+      next();
+  } catch (e) {
+      next();
+      return;
+  }    
 }
 
 module.exports = JWTMiddleware;
--- middlewares/adminOnlyGuard.js
+++ middlewares/adminOnlyGuard.js
@@ -1,12 +1,12 @@
 const adminOnlyGuard = (request, response, next) => {
-    const isAdmin = request.tokenPayload.role === 'ADMIN';
+  const isAdmin = request.tokenPayload.role === 'ADMIN';
 
-    if(!isAdmin) {
-        response.status(403).send({ 'message': 'Not Allowed'});
-        return;
-    } else {
-        next();
-    }
+  if(!isAdmin) {
+      response.status(403).send({ 'message': 'Not Allowed'});
+      return;
+  } else {
+      next();
+  }
 }
 
 module.exports = adminOnlyGuard;
